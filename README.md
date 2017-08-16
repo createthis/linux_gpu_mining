85a0a65a96e274647e4f420708fedea656bd34fc
Jesse CreateThis <jesse@createthis.com>
2017-08-16 15:57:10 -0400

    Add deps

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## First time setup
 
 ```bash
+./install_dependencies.sh # git, vim, and chrome
 ./enable_nvidia_fan_control.sh
 ./restart_ubuntu_lightdm.sh
 ```
---
+++ install_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update 
+sudo apt-get install git vim google-chrome-stable
